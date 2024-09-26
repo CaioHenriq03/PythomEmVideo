eed291c35c90ddaa09f7b7053e29d01e50ffd17d
Caio Henrique <kaka_3551@hotmail.com>
2024-09-26 09:40:47 -0300

    commit

---
+++ exercicios/exerc88.py
@@ -0,0 +1,25 @@
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+print('-' * 30)
+print('       JOGA NA MEGA SECTION      ')
+print("-" * 30)
+quant = int(int('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+            if cont >= 6:
+                break
+lista.sort()
+jogos.append(lista[:])
+lista.clear()
+tot += 1
+print('-=' * 3, f' SORTEANDO {quant} JOGOS ', '-=' * 3)
+for i, l in enumerate(jogos):
+    sleep(1)
